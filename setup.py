c36b45454e3401d24183ddf941e45c461323aa45
lucyking <joehisaishi1943@gmail.com>
2016-07-29 16:38:36 +0800

    fix pkg install path
    Signed-off-by: lucyking <joehisaishi1943@gmail.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # coding: utf-8
 import setuptools
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+setuptools.setup(setup_requires=['pbr'], pbr=True, package_dir={'': 'src'}, packages=["AircvLibrary"])
